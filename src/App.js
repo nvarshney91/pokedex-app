b04bfdacdd978473dad5765df5f1849411fb2014
nvarshney91 <varshneynishant0@gmail.com>
2022-05-24 18:50:04 +0530

    Adding Pokedex Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
+import './css/Pokecard.css'
+import Pokecard from './component/Pokecard';
+import Pokedex from './component/Pokedex';
+import Pokegame from './component/Pokegame';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Pokegame />
     </div>
   );
 }
-
 export default App;
---
+++ src/component/Pokecard.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+// const PokeAPI='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const PokeAPI='https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padToThree=(num)=>((num<=999)? `00${num}`.slice(-3):num);
+class Pokecard extends Component{
+    render()
+    {
+        let imgSrc=`${PokeAPI}${padToThree(this.props.id)}.png`;
+         return ( 
+         <div className="Pokecard">
+         <h1 className='Pokecard-title'>
+             {this.props.name}
+         </h1>
+         <div className="Pokecard-image"><img src={imgSrc} alt={this.props.name}/></div>
+         <div className="Pokecard-data">Type: {this.props.type}</div>
+         <div className="Pokecard-data">EXP: {this.props.exp}</div>
+         </div>
+         );
+    }
+}
+ 
+export default Pokecard;
